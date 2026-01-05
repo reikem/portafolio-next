55cdce49fedd3a19647ec2efb704d07121cae4a1
reikemdev <jimmymeneses11@gmail.com>
2026-01-04 22:55:10 -0300

    add interface proyect

---
+++ portafolio-next/components/interfaces/index.ts
@@ -0,0 +1,26 @@
+export interface IProject{
+    id:number;
+    title:string;
+    description:string;
+    image:string;
+    tags:string[];
+    fullDescription:string;
+    demoUrl:string;
+    githubUrk:string;
+    color:string;
+    category:string;
+}
+
+export interface ISkill{
+    name:string;
+    level:number;
+    category:string;
+    icon:string;
+}
+export interface IEducation{
+    title:string;
+    institution:string;
+    year:string;
+    description:string;
+    type:string;
+}
